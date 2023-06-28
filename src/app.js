5c3c7e1c6b309d63aea4206497743399f5d44ac5
lalauo <87227789+lalauo@users.noreply.github.com>
2023-06-28 16:17:23 +0100

    Removed unit conversion, and updated layout

--- src/app.js
+++ src/app.js
@@ -115,30 +115,6 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-// Unit Conversion
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-
-  let fahrenheitTemperature = Math.round((celsiusTemperature * 9) / 5 + 32);
-
-  document.querySelector("#current-temperature").innerHTML =
-    fahrenheitTemperature;
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-
-  document.querySelector("#current-temperature").innerHTML =
-    Math.round(celsiusTemperature);
-}
-
 // Forecast
 
 function formatDay(timestamp) {
@@ -195,15 +171,6 @@ function displayForecast(response) {
 }
 
 // Global
-let celsiusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let form = document.querySelector("form#city-search");
 form.addEventListener("submit", submitInput);
